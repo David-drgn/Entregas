4f50e6ad4c8d785f92a695fe6087c791e4d0b0f6
David_drgn <72632974+David-drgn@users.noreply.github.com>
2023-09-25 20:10:03 -0300

    -- Tempo de execução

--- Avaliação_Bimestral_N3/ArvoreBinaria/src/main/SaidaDados.java
+++ Avaliação_Bimestral_N3/ArvoreBinaria/src/main/SaidaDados.java
@@ -1,68 +1,78 @@
 package main;
 
 import javax.swing.JOptionPane;
+import javax.swing.Spring;
 
 public class SaidaDados {
-	
-	static void saidaAdicional(No no) {
+
+	static String saidaDadosEmOrdem(No no, String result) {
 		if (no != null) {
-
-			saidaAdicional(no.getEsquerda());
-			saidaAdicional(no.getDireita());
-
-			int valEsquerda = 0;
-			int valDireita = 0;
-
-			try {valEsquerda = no.getEsquerda().getVal();} catch (Exception e) {valEsquerda = 0;}
-
-			try {valDireita = no.getDireita().getVal();} catch (Exception e) {valDireita = 0;}
-
-			JOptionPane.showMessageDialog(null, "Valor nó principal: " + no.getVal() + "\nEsquerda: " + valEsquerda + " Direita: " + valDireita);
+			result = "" + saidaDadosEmOrdem(no.esquerda, result) + " " + no.getVal() + " "
+					+ saidaDadosEmOrdem(no.direita, result);
+			return result;
 		}
+		return "";
+	}
+
+	static String saidaDadosPreOrdem(No no, String result) {
+		if (no != null) {
+			result = "" + no.getVal() + " " + saidaDadosPreOrdem(no.esquerda, result) + " "
+					+ saidaDadosPreOrdem(no.direita, result);
+			return result;
+		}
+		return "";
+	}
+
+	static String saidaDadosPosOrdem(No no, String result) {
+		if (no != null) {
+			result = "" + saidaDadosPosOrdem(no.esquerda, result) + " " + saidaDadosPosOrdem(no.direita, result) + " "
+					+ no.getVal();
+			return result;
+		}
+		return "";
 	}
 	
-	static void saidaDadosEmOrdem(No no) {
-        if (no != null) {
-        	saidaDadosEmOrdem(no.esquerda);
-        	JOptionPane.showMessageDialog(null, "Valor nó: " + no.getVal());
-        	saidaDadosEmOrdem(no.direita);
-        }
-    }
+	static long time() {
+		return System.nanoTime();
+	}
 	
-	static void saidaDadosPreOrdem(No no) {
-        if (no != null) {
-        	JOptionPane.showMessageDialog(null, "Valor nó: " + no.getVal());
-        	saidaDadosPreOrdem(no.esquerda);
-        	saidaDadosPreOrdem(no.direita);
-        }
-    }
-	
-	static void saidaDadosPosOrdem(No no) {
-        if (no != null) {
-        	saidaDadosPosOrdem(no.esquerda);
-        	saidaDadosPosOrdem(no.direita);
-            JOptionPane.showMessageDialog(null, "Valor nó: " + no.getVal());
-        }
-    }
-
+	static double tempoDeExecucao(long tempoInicial, long tempoFinal) {
+		long dif = tempoFinal - tempoInicial;
+		double ms = (double) dif / 1_000_000.0;
+		return ms;
+	}
 
 	static void selectOrder(No no) {
 		String valNo1 = JOptionPane.showInputDialog(null, "Digite a ordem: " + "\n1: para em ordem"
-				+ "\n2: Para pré-ordem" + "\n3: Para pós-ordem" + "\n4: Para ver nó por nó"  + "\n0: Para sair");
+				+ "\n2: Para pré-ordem" + "\n3: Para pós-ordem" + "\n0: Para sair");
 		int val = 0;
 		try {
 			val = Integer.parseInt(valNo1);
 		} catch (Exception e) {
 			selectOrder(no);
 		}
-		if (val == 1)
-			saidaDadosEmOrdem(no);
-		if (val == 2)
-			saidaDadosPreOrdem(no);
-		if (val == 3)
-			saidaDadosPosOrdem(no);
-		if (val == 4)
-			saidaAdicional(no);
+		if (val == 1) {
+			
+			long tempoInicial = time();
+			String result = saidaDadosEmOrdem(no, "");
+			long tempoFinal = time();
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + result + "\nTempo de execução em milesimos: " + tempoDeExecucao(tempoInicial, tempoFinal));
+		}
+		if (val == 2) {
+			long tempoInicial = time();		
+			String result = saidaDadosPreOrdem(no, "");
+			long tempoFinal = time();
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + result + "\nTempo de execução em milesimos: " + tempoDeExecucao(tempoInicial, tempoFinal));
+		}
+		if (val == 3) {
+			long tempoInicial = time();		
+			String result = saidaDadosPosOrdem(no, "");
+			long tempoFinal = time();
+			
+			JOptionPane.showMessageDialog(null, "Resultado: " + result + "\nTempo de execução em milesimos: " + tempoDeExecucao(tempoInicial, tempoFinal));
+		}
 		if (val == 0)
 			return;
 		else
